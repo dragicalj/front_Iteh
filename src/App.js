90e98ab88a8b9a2faa7f3738bfd833ee6dad43cd
dragicalj <gagaljubisavljevic111@gmail.com>
2022-05-04 17:51:37 +0200

    Poslat zahtev za login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+class App extends React.Component {
+  
+  
+  
+  constructor(props) {
+    super(props);
+
+    this.state={
+      access_token:null,
+      refresh_token:null
+    }
+  }
+
+  
+
+  componentDidMount() {
+    // Simple POST request with a JSON body using fetch
+    fetch("http://localhost:8090/api/login", {
+  method: 'POST',
+  headers: new Headers({
+             'Content-Type': 'application/x-www-form-urlencoded', // <-- Specifying the Content-Type
+    }),
+  body: "username=john&password=1234" // <-- Post parameters
+})
+.then((response) => {response.text())
+.then((responseText) => {
+  console.log(responseText);
+}).then(data => this.setState({ access_token: data.t, refresh_token: data.re })
+.catch((error) => {
+    console.error(error);
+}));
+}
+    
+  
+  render(){
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> GAGICAAAAAA
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <button onClick={this.componentDidCatch}> Click me!{this.state} </button>
     </div>
   );
+}
 }
 
 export default App;
