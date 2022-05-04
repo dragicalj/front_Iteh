06122c9f0596c5daaaf41446935bda39ba4bfc35
Jazz-NZ <56455946+Jazz-NZ@users.noreply.github.com>
2022-05-04 18:22:31 +0200

    Getting access and refresh token

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
 
     this.state={
       access_token:null,
-      refresh_token:null
+      refresh_token : null
     }
   }
 
@@ -19,29 +19,30 @@ class App extends React.Component {
 
   componentDidMount() {
     // Simple POST request with a JSON body using fetch
-    fetch("http://localhost:8090/api/login", {
+    fetch("http://localhost:8080/api/login", {
   method: 'POST',
   headers: new Headers({
              'Content-Type': 'application/x-www-form-urlencoded', // <-- Specifying the Content-Type
     }),
   body: "username=john&password=1234" // <-- Post parameters
 })
-.then((response) => {response.text())
-.then((responseText) => {
-  console.log(responseText);
-}).then(data => this.setState({ access_token: data.t, refresh_token: data.re })
+.then(response => response.json())
+        .then(data => this.setState({ access_token: data.access_token, refresh_token: data.refresh_token }))
 .catch((error) => {
     console.error(error);
-}));
+});
 }
     
   
   render(){
-  
+    const { access_token, refresh_token } = this.state;
   return (
     <div className="App">
-      
-      <button onClick={this.componentDidCatch}> Click me!{this.state} </button>
+      <h1>
+        {console.log(access_token)}
+        {console.log(refresh_token)}
+      </h1>
+      <button onClick={this.componentDidCatch}> Click me! </button>
     </div>
   );
 }
