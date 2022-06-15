f42e07aa6d39efc70841cdcd79d8f04c782b73a5
dragicalj <gagaljubisavljevic111@gmail.com>
2022-06-15 17:33:20 +0200

    izmena u home

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import {Link } from "react-router-dom";
+import Login from './Login';
 
 
 function Home() {
+  if(!token){
+    return<Login setToken={setToken}/>
+  }
+  if(token){
+    return <Home/>
+  }
   return (
       <>
     <div>Home</div></>
   )
 }
 
-export default Home
+export default Home;
