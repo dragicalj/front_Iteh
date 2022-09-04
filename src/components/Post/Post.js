aa3b9f7b6db7a8cdae0f8aafd5de63ddf1fc7190
dragicalj <gagaljubisavljevic111@gmail.com>
2022-09-04 12:57:44 +0200

    delete post

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,15 +1,47 @@
 import React from 'react'
 import './Post.css';
+import { useState } from 'react';
 
 
 
 function Post(post) {
+
+    var isDeleted = false;
+    const[deletePostID, setDeletePostID] = useState("");
 
   React.useEffect(() => {
     console.log("OVDE SU INFO O POST")
     console.log(post)
   }, []);
 
+  const callDeletePost = async e => {
+    e.preventDefault(); 
+    await deletePost(post.post.id);
+    if(isDeleted) {
+      alert("Post deleted!");
+    } else {
+      alert("Post doesn't exist");
+    }
+    window.location.reload(false);
+  }
+
+  async function deletePost(postId) {
+    return fetch('http://localhost:8090/api/post/delete/'+ postId, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${JSON.parse(localStorage.getItem("token"))}`,
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods':'POST,GET,DELETE,PATCH,OPTIONS'
+      },
+    })
+      .then(function(response) {
+        if(response.ok) {
+          isDeleted = true;
+        } else {
+          isDeleted = false;
+        }
+      })
+  }
   return (
     <div style={{width : "1000px", marginTop: "50px"}} className="container">
     <div className="col-md-12 col-lg-12">
@@ -38,7 +70,7 @@ function Post(post) {
                     <div className="caption">
                         <h3 className="md-heading"><a href="#">{post.post.title}</a></h3>
                         <p> {post.post.description} </p>
-                        {/* <a className="btn btn-default" href="#" role="button">Read More</a>  */}
+                        { <a className="delete-btn" href="#" role="button" onClick={callDeletePost}>Delete post</a>  }
                     </div>
                 </div>
             </div>
