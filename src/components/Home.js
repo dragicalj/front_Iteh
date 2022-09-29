f010fe66d0a18e9876ccbeece9d50ef19c4dbf10
Jazz-NZ <56455946+Jazz-NZ@users.noreply.github.com>
2022-09-29 21:37:06 +0200

    Home page nav bar refactoring

--- src/components/Home.js
+++ src/components/Home.js
@@ -53,66 +53,11 @@ function Home() {
     setIsLoaded2(true);
   };
 
-  const callCreateGroup = async (e) => {
-    e.preventDefault();
-    const groupInformations = await createGroup(groupName);
-    localStorage.setItem(
-      "groupInformations",
-      JSON.stringify(groupInformations)
-    );
-    alert("Group '" + groupName + "'created!");
-    const joinResponse = await joinGroup(
-      JSON.parse(localStorage.getItem("username")),
-      groupName
-    );
-    window.location.reload(false);
-  };
-
-  const callDeleteGroup = async (e) => {
-    e.preventDefault();
-    await deleteGroup(
-      deleteGroupName,
-      JSON.parse(localStorage.getItem("username"))
-    );
-    if (isDeleted) {
-      alert("Group '" + deleteGroupName + "' deleted!");
-    } else {
-      alert("Group : '" + deleteGroupName + "' doesn't exist");
-    }
-    window.location.reload(false);
-  };
-
-  const callUpdateUser = async (e) => {
-    e.preventDefault();
-    await updateUser(JSON.parse(localStorage.getItem("username")), newUserName);
-    if (isUpdated1) {
-      alert("User name changed!");
-    } else {
-      alert("Unsuccessful! Try again!");
-    }
-    window.location.reload(false);
-  };
-
-  const callJoinGroup = async (e) => {
-    e.preventDefault();
-    await joinGroup(
-      JSON.parse(localStorage.getItem("username")),
-      joinGroupName
-    );
-    if (isJoined) {
-      alert("successfully joined in '" + joinGroupName + "' group");
-    } else {
-      alert("Group : '" + joinGroupName + "' doesn't exist");
-    }
-    window.location.reload(false);
-  };
-
   if (isLoaded && isLoaded2) {
     return (
      
       <div style={{ textAlign: "left", width: "100%" }}>
-         <HomeNavBar></HomeNavBar>
-        <NavBar></NavBar>
+        <HomeNavBar></HomeNavBar>
         <div class="row">
           <div
             class="col"
@@ -139,112 +84,6 @@ function Home() {
             <div
               style={{ marginTop: "40px", marginLeft: "10px", width: "300px" }}
             >
-              <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label style={{ fontWeight: "bold" }}>
-                    Create new group
-                  </Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Enter group name"
-                    onChange={(e) => setGroupName(e.target.value)}
-                  />
-                  <Form.Text className="text-muted">
-                    Create new group and share posts with your friends.
-                  </Form.Text>
-                </Form.Group>
-                <button
-                  className="btn btn-primary"
-                  style={{ width: "300px" }}
-                  type="submit"
-                  onClick={callCreateGroup}
-                >
-                  Create group
-                </button>
-              </Form>
-            </div>
-
-            <div
-              style={{ marginTop: "40px", marginLeft: "10px", width: "300px" }}
-            >
-              <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label style={{ fontWeight: "bold" }}>
-                    Join group
-                  </Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Enter group name"
-                    onChange={(e) => setJoinGruopName(e.target.value)}
-                  />
-                  <Form.Text className="text-muted">
-                    Join to group and share posts with your friends.
-                  </Form.Text>
-                </Form.Group>
-                <button
-                  className="btn btn-primary"
-                  style={{ width: "300px" }}
-                  type="submit"
-                  onClick={callJoinGroup}
-                >
-                  Join group
-                </button>
-              </Form>
-            </div>
-
-            <div
-              style={{ marginTop: "40px", marginLeft: "10px", width: "300px" }}
-            >
-              <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label style={{ fontWeight: "bold" }}>
-                    Delete group
-                  </Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Enter group name"
-                    onChange={(e) => setDeleteGroupName(e.target.value)}
-                  />
-                  <Form.Text className="text-muted">
-                    Delete group if you think that there is anymore to talk
-                    about.
-                  </Form.Text>
-                </Form.Group>
-                <button
-                  className="btn btn-primary"
-                  style={{ width: "300px" }}
-                  type="submit"
-                  onClick={callDeleteGroup}
-                >
-                  Delete group
-                </button>
-              </Form>
-            </div>
-
-            <div
-              style={{ marginTop: "40px", marginLeft: "10px", width: "300px" }}
-            >
-              <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label style={{ fontWeight: "bold" }}>
-                    Change your name
-                  </Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Enter name"
-                    onChange={(e) => setNewUserName(e.target.value)}
-                  />
-                  <Form.Text className="text-muted"></Form.Text>
-                </Form.Group>
-                <button
-                  className="btn btn-primary"
-                  style={{ width: "300px" }}
-                  type="submit"
-                  onClick={callUpdateUser}
-                >
-                  Save changes
-                </button>
-              </Form>
             </div>
           </div>
           <div class="col-8" style={{ marginTop: "50px" }}>
@@ -274,63 +113,6 @@ function Home() {
         </h1>
       </div>
     );
-  }
-
-  async function joinGroup(username, groupname) {
-    return fetch("http://localhost:8090/api/group/add", {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`,
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username: username, groupname: groupname }),
-    }).then(function (response) {
-      if (response.ok) {
-        console.log("Uspesno joinovan!");
-        isJoined = true;
-      } else {
-        isJoined = false;
-      }
-    });
-  }
-
-  async function deleteGroup(groupname, username) {
-    return fetch(
-      "http://localhost:8090/api/group/" + groupname + "/" + username,
-      {
-        method: "DELETE",
-        headers: {
-          Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`,
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "POST,GET,DELETE,PATCH,OPTIONS",
-        },
-      }
-    ).then(function (response) {
-      if (response.ok) {
-        isDeleted = true;
-      } else {
-        isDeleted = false;
-      }
-    });
-  }
-  async function updateUser(username, newName) {
-    return fetch("http://localhost:8090/api/user/update", {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`,
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username: username, newName: newName }),
-    }).then(function (response) {
-      if (response.ok) {
-        console.log("Uspesno promenjeno ime!");
-        isUpdated1 = true;
-      } else {
-        isUpdated1 = false;
-      }
-    });
   }
 }
 
